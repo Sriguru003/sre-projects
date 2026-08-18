8e9e1a6c101772584addff5655710ebee6e9ba80
Guruyadav <guruyadav.003@gmail.com>
2026-08-18 23:23:55 +0530

    new update

--- Progress.sh
+++ Progress.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-cd /home/guruyadav/sre-projects
-git add .
-git commit -m "new update"
-git status
-echo output
-git push
-echo "push completed"
+echo "Started"
 
+cd /home/guruyadav/sre-projects
+
+git_status=$(git status)
+
+echo "$git_status"
+
+if echo "$git_status" | grep -q "nothing to commit, working tree clean"; then
+    echo "No changes found. Exiting..."
+    exit 0
+elif echo "$git_status" | grep -q "Untracked files"; then
+    echo "Untracked files found. Continuing..."
+else
+    echo "Changes found. Continuing..."
+fi
+
+git add .
+
+git commit -m "new update"
+
+git push
+
+echo "Push completed successfully"
